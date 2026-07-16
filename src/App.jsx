00e494abb8602c0adbc18d8de40052123f640b3d
ndemanou237 <ndemanoudonald9@gmail.com>
2026-07-16 22:12:05 +0100

    initialisation in the react

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 
+const title = "Bonjour a vous tous chez citoyen"
+const test = {color:'red', backgroundColor:'aqua'}
 
 function App() {
-  return 'bonjour les gens'
+
+  const clicked = (e) =>{
+    console.log(e)
+    alert('vous avez cliquez sur le h1')
+  }
+  return  <div>
+    <h1 onClick={clicked} id='title' className='title' style={test}>{title}</h1>
+    <input type="text" />
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem voluptates enim dicta, molestias ullam tenetur corporis ipsum porro quisquam architecto provident quia quidem. Illo consequuntur animi reiciendis quod maiores magnam.</p>
+
+  </div>
+  
 }
 
 export default App
