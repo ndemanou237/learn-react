ff501e048e67640b4a91fce9480d5972cb1b1798
ndemanou237 <ndemanoudonald9@gmail.com>
2026-07-16 08:48:43 +0100

    initialisation the formation

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+
+
+function App() {
+  return 'bonjour les gens'
+}
+
+export default App
